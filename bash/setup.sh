bcdddfd19bbd472cf555ec59c1c7ebccc6856a07
johndeerdoe <airsoftgunman2@gmail.com>
2022-05-16 23:11:14 -0500

    changed structure a little, added .js and .py files for starters

---
+++ bash/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+### update and upgrade
+sudo apt update && upgrade
+wait 1
+###
+sudo apt install neofetch
--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-### update and upgrade 
-sudo apt update && upgrade 
-wait 1
-###
-sudo apt install neofetch
